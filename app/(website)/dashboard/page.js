60c181e1cb865c3f22cddd2729305937c16872ae
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-08-10 15:16:09 -0400

    create user hook

--- app/(website)/dashboard/page.js
+++ app/(website)/dashboard/page.js
@@ -1,37 +1,28 @@
 import React from 'react'
 import { currentUser } from '@clerk/nextjs/server'
-import prisma from '@/lib/prisma'
+import { createUser } from '@/lib/helper/create-user'
+import { UserButton } from '@clerk/nextjs'
 
 const DashboardPage = async () => {
     const user = await currentUser()
 
-    const userEmail = user.emailAddresses[0].emailAddress
-    const userName = user.firstName || "Unknown" 
+    const userEmail = user?.emailAddresses[0].emailAddress
+    const userName = user?.firstName || "Unknown" 
 
-    // Check if the user already exists in the database
-    let existingUser = await prisma.user.findUnique({
-        where: {
-            email: userEmail,
-        },
-    })
-
-    // If the user doesn't exist, create a new user
-    if (!existingUser) {
-        existingUser = await prisma.user.create({
-            data: {
-                email: userEmail,
-                name: userName,
-            },
-        })
+    // Create a user if no user
+    try {
+        const existingUser = await createUser(userEmail, userName)
+        console.log("Existing User: ", existingUser)
+    } catch (error) {
+        console.error("Error in DashboardPage:", error)
     }
-
-    console.log("Existing User: ", existingUser)
 
     return (
         <div>
+            <UserButton />
             <h1>Dashboard</h1>
-            <p>Welcome, {existingUser.name}!</p>
-            <p>Your email: {existingUser.email}</p>
+            {/* <p>Welcome, {existingUser?.name}!</p>
+            <p>Your email: {existingUser?.email}</p> */}
         </div>
     )
 }
